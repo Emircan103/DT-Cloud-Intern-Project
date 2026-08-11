76885286bc9a70b40b87b2e90d14a69107043372
Emircan103 <emircanakturk35@hotmail.com>
2026-08-11 19:12:26 +0300

    çalışan database

--- Backend/src/lib/prisma.ts
+++ Backend/src/lib/prisma.ts
@@ -1,12 +1,19 @@
-import 'dotenv/config'; // .env değişkenlerinin prisma.ts içinde de okunmasını sağlar
-
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 const adapter = new PrismaPg(pool);
 
-const prisma = new PrismaClient({ adapter });
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
